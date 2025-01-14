d0a188b4e935808192e6511d9b90148dd9feb0dd
Pro100D <114569004+Pro100D@users.noreply.github.com>
2025-01-14 21:00:33 +0100

    fix

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,3 +2,5 @@ import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://127.0.0.1:8000';
 axios.defaults.baseURL = 'https://cooking-book0bd.onrender.com';
+
+// json str 5 "homepage": "https://Pro100D.github.io/kitchenList",
